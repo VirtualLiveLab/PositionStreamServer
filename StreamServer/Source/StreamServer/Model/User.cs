ba5048010f8bafffb57e33120f01dd984884d431
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2020-08-25 16:41:21 +0900

    fix

--- StreamServer/Source/StreamServer/Model/User.cs
+++ StreamServer/Source/StreamServer/Model/User.cs
@@ -8,7 +8,7 @@ namespace StreamServer.Model
         public readonly string UserId;
         public volatile MinimumAvatarPacket? CurrentPacket;
         public volatile IPEndPoint? RemoteEndPoint;
-        public volatile DateTimeBox DateTimeBox;
+        public volatile DateTimeBox? DateTimeBox;
 
         public User(string userId)
         {
