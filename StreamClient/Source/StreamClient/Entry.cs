b1cf443c62a1e9d1dd29567127522f5a596c0b53
Kouichi Matsumura <kai4.1.1.2@gmail.com>
2020-10-25 09:09:57 +0000

    Update to pass ipaddress as command line args

--- StreamClient/Source/StreamClient/Entry.cs
+++ StreamClient/Source/StreamClient/Entry.cs
@@ -12,11 +12,13 @@ namespace StreamClient
         static async Task Main(string[] args)
         {
             var str = args.Length > 0 ? args[0] : Console.ReadLine();
+            var ipaddr = args.Length > 1 ? args[1] : "127.0.0.1";
+            Console.WriteLine(ipaddr);
             var k = Int32.Parse(str);
             var ctsList = new List<CancellationTokenSource>();
             for (int i = k; i < 200 + k; ++i)
             {
-                UdpClient udpClient = UdpClientFactory.CreateClient("127.0.0.1", 5577);
+                UdpClient udpClient = UdpClientFactory.CreateClient(ipaddr , 5577);
                 var position = new Vector3(i%100*4, 0.5f, i/100*2);
                 var output = new OutputLoop(udpClient, 16, $"user{i}", position);
                 output.Start();
